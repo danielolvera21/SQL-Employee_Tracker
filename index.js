aadd535abf598f3a5a6f804da3992adc827b19a4
Daniel O <danielolvera2188@gmail.com>
2021-09-21 14:08:24 -0600

    updated README

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const promptUser = () => {
             case 'View all employees':
                 viewAllEmployees();
                 break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
             default:
                 console.log("Exiting")
                 process.exit();
@@ -44,11 +50,63 @@ const viewAllRoles = () => {
 }
 
 const viewAllEmployees = () => {
-    connection.query('SELECT * FROM employees', (err, res) => {
+    connection.query('SELECT * FROM employee', (err, res) => {
         console.table(res);
         promptUser();
     })
 }
+
+const addDepartment = () => {
+    inquirer.prompt({
+        type: "input",
+        name: "department",
+        message: "What's the department name you would like to add?"
+    })
+        .then(data => {
+            const newDepartment = data.department
+            connection.query(`INSERT INTO department SET ?`, { name: newDepartment }, (err, res) => {
+                if (err) throw err;
+                // console.table(res);
+                viewAllDepartments();
+            })
+        })
+
+}
+
+const addRole = () => {
+    connection.query('SELECT * FROM department', (err, res) => {
+        const departmentArr = []
+        console.log(typeof res);
+        const x = res.map((department, index) => {
+            console.log(department, index)
+            departmentArr.push(department.id + ". " + department.name)
+        })
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "What's the role title you would like to add?"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What's the role salary you would like to add?"
+            },
+            {
+                type: "list",
+                name: "department",
+                message: "Please choose a department",
+                choices: departmentArr
+            }
+        ])
+            .then(data => {
+                console.log(data)
+                const departmentId = data.department.split(". ")[0]
+                console.log(departmentId)
+            })
+    })
+}
+
 
 connection.connect(err => {
     if (err) throw err;
